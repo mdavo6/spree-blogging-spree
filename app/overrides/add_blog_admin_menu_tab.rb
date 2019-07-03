3a91d9805facf668319b1af22577b04357475aa9
Michael Davidson <mdavo6@gmail.com>
2019-07-03 22:53:34 +1000

    Test

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -3,6 +3,3 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :insert_bottom => "#main-sidebar",
                      :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab Spree.t(:BLOG), url: spree.admin_blog_entries_path, icon: 'comment' %></ul><% end %>",
                      :disabled => false)
-
-
-
