d554ab98c0d147fe20d529a777600686c7de76f5
Michael Davidson <mdavo6@gmail.com>
2021-04-30 16:24:41 +1000

    Update controllers from class_eval to module.prepend

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -1,3 +1,7 @@
-Spree::StoreController.class_eval do 
-  helper 'spree/blog_entries'
+module Spree
+  module StoreControllerDecorator
+    helper 'spree/blog_entries'
+  end
 end
+
+::Spree::StoreController.prepend(Spree::StoreControllerDecorator)
