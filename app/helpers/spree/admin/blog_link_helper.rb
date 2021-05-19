89a10becda8a0742bb8d1e5b562f98d57ef44c87
Michael Davidson <mdavo6@gmail.com>
2021-05-19 14:15:08 +1000

    Rename navigation_helper_decorator to blog_link_helper

---
+++ app/helpers/spree/admin/blog_link_helper.rb
@@ -0,0 +1,11 @@
+module Spree
+  module Admin
+    module BlogLinkHelper
+      def link_to_blog_in_store(resource, options={})
+        url = options[:url] || blog_entry_permalink(resource)
+        #options[:data] = {:action => 'show'}
+        link_to(Spree.t(:view_in_store), url, options)
+      end
+    end
+  end
+end
