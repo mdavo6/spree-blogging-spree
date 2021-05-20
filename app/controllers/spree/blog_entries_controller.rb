1fe677c9e0d8ee144ba19afc5a3439b1974606db
Michael Davidson <mdavo6@gmail.com>
2021-05-20 10:34:24 +1000

    Update before_filter to before_action

--- app/controllers/spree/blog_entries_controller.rb
+++ app/controllers/spree/blog_entries_controller.rb
@@ -1,9 +1,9 @@
 class Spree::BlogEntriesController < Spree::StoreController
   before_action :new_subscriber
-  
+
   helper 'spree/blog_entries'
 
-  before_filter :init_pagination, :only => [:index, :tag, :archive, :author, :category]
+  before_action :init_pagination, :only => [:index, :tag, :archive, :author, :category]
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def index
